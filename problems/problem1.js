df8ca12e305b52d63caa1c33cae2e5abb4b7628a
Huy Huynh <huynhphuchuy@live.com>
2019-08-17 14:43:42 +0700

    remove comments

--- problems/problem1.js
+++ problems/problem1.js
@@ -7,14 +7,14 @@
 const rotate = (grid, k) => {
     k %= 4;
     if (k === 4 || k === 0) return grid;
-    const rotated = grid.map((g, i) => { // {1} // O(n-1)
+    const rotated = grid.map((g, i) => { // {1}
         const dummy = [];
-        for (let j = 0; j < grid.length; j++) { // {2} // O(n-1)
+        for (let j = 0; j < grid.length; j++) { // {2}
             dummy.push(grid[j][i])
         }
         return dummy.reverse() // {3}
     })
-   return rotate(rotated, k - 1); // {4} // O(k)
+   return rotate(rotated, k - 1); // {4}
 }
 
 module.exports = rotate;
